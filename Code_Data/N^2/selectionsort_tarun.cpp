ebe5a5bc8f88e7cafae9282139429dcf94a9d0e2
tarungupta1729 <tarungupta1729@gmail.com>
2018-02-22 22:14:34 +0530

    created n^2 cpp and out files

--- Code_Data/N^2/selectionsort_tarun.cpp
+++ Code_Data/N^2/selectionsort_tarun.cpp
@@ -1,17 +1,24 @@
 #include <bits/stdc++.h>
-const int MAXN = 1e6  ;
+const int MAXN = 1e4  ;
 #include <algorithm>
 #include <vector>
 using namespace std;
-bool linear(vector<int> A,int n,int x)
+void selectionSort(vector<int>  &A, int n)
 {
-    for(int i=0;i<n;i++)
-    {
-        if(A[i]==x)
-        return true;
+    int i, j, min_idx;
 
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+          if (A[j] < A[min_idx])
+            min_idx = j;
+
+        // Swap the found minimum element with the first element
+        swap(A[min_idx], A[i]);
     }
-    return false;
 }
 int main(  )
  {
@@ -26,39 +33,40 @@ int main(  )
 
 
    //  int n ;
-  freopen("linearsearch_tarun.out", "w", stdout);
-  for( int i  = 0 ;  i  <  5000 ; i ++ )
+  freopen("selectionsort_tarun.out", "w", stdout);
+  for( int i  = 0 ;  i  <  5000; i ++ )
    {
 
       int N  = rand()%MAXN ;  // output meib ibckude kr
-      
+
 
 
       /* This is the initialisation part */
 
 
-      if( i < 50 )N = rand()%100 + 1 ;
-      vector<int>A( N ) ;
+      if( i < 50 )N = rand()%(int)(100) + 1 ;
+
+       vector<int> A( N ) ;
       for( int i = 0 ; i < N ; i ++ )
         {
           A[i] = rand()%(int)( 1e9 )   ;
          }
 
-        int x = rand()%(int)(1e9);
       /* This is the initialisation part */
-
+    //cout<<N<<" ";
 
      /* code computaion part */
        struct Node* root  = NULL ;
        const clock_t b1 = clock();
 
-        linear(A,N,x);
+            selectionSort(A,N);
+
 
        double  k =  float( clock () - b1 ) /  CLOCKS_PER_SEC; // output meib ibckude kr
 
        /* code computaion part */
 
-
+        //print(A,N);
 
        printf("%d,%.12f,%.12f\n",N,k,processor_unit_time);
 
