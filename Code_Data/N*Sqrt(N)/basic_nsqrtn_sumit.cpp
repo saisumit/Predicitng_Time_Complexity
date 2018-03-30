6bcc8b5f66d08d34746dde2d48be1d624224bc10
saisumit <sumitvohra90@gmail.com>
2018-03-30 16:22:24 +0530

    added nsqrtn codes

---
+++ Code_Data/N*Sqrt(N)/basic_nsqrtn_sumit.cpp
@@ -0,0 +1,34 @@
+
+#include <bits/stdc++.h>
+const int MAXN = 1e9;
+using namespace std;
+
+int main()
+{
+    const clock_t time_st = clock();
+    for(int i=0;i<=MAXN;i++);
+    double k =  float( clock () - time_st )/CLOCKS_PER_SEC;
+    double processor_unit_time  =  k/MAXN ;
+    freopen("basic_nsqrtn_sumit.out","w",stdout);
+  
+    for(int i=1;i<=5000;i++)
+    {
+        int n ; 
+        if(i<=50)
+            n = rand()%(int)(100);
+        else
+            n = rand()%(int)(1e5);
+        
+        
+        const clock_t start_t = clock();
+        
+        int cnt = 0 ; 
+        for( int i =1 ; i < n; i ++ )
+        	for( int j = 1 ; j < sqrt(n); j ++ )
+             		 	cnt ++ ; 
+        double  p =  float( clock () - start_t ) /  CLOCKS_PER_SEC;
+
+        printf("%d,%.12f,%.12f\n",n,p,processor_unit_time);
+    }
+    return 0;
+}
